b854daa3de0e175a5b176698ee8c37c6bd1098e8
jackhli <jackhli@tencent.com>
2024-05-13 16:52:24 +0800

    feat: add split

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -25,7 +25,11 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<C-h>', 'require("smart-splits").move_cursor_left)', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', 'require("smart-splits").move_cursor_down)', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', 'require("smart-splits").move_cursor_right)', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', 'require("smart-splits").move_cursor_up)', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<A-h>', 'require("smart-splits").resize_left', { desc = 'Resize left' })
+vim.keymap.set('n', '<A-l>', 'require("smart-splits").resize_right', { desc = 'Resize right' })
+vim.keymap.set('n', '<A-j>', 'require("smart-splits").resize_down', { desc = 'Resize down' })
+vim.keymap.set('n', '<A-k>', 'require("smart-splits").resize_up', { desc = 'Resize up' })
